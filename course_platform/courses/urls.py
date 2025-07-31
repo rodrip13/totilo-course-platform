7aafb94873782524ce6163814aa7921bb5ae6aa7
rodrip13 <rodrip137@gmail.com>
2025-07-31 16:23:32 -0300

    page funcional, necesita muchos cambios. es la primer version

--- course_platform/courses/urls.py
+++ course_platform/courses/urls.py
@@ -5,6 +5,7 @@ app_name = 'courses' # Add this line
 
 urlpatterns = [
     path('', views.home_view, name='home'),
+    path('courses/home', views.course_list_view, name='course_list'),
     path('courses/', views.course_list_view, name='course_list'),
     path('courses/<int:course_id>/', views.course_detail_view, name='course_detail'),
     path('register/', views.register_view, name='register'), # Added registration URL
